873fc6cbe39241e5fa134cc049ec9cad9b008efb
SandeepRana <38207659+SandeepRana1001@users.noreply.github.com>
2022-01-09 15:22:04 +0530

    Basic Of Node

---
+++ basics/app.js
@@ -0,0 +1,20 @@
+const userName = "Sandeep"
+//importing file system module
+const fs = require('fs')
+
+// To run this file use  - node app.js
+
+// This statement below will print the value of userName
+//console.group(userName)
+
+// This alert statement below won't work as it is not supported by Node Js
+// alert("Go")    
+
+// This statement below will create a file in the same directory
+fs.writeFile('user-data.txt', 'username : ' + userName, (err) => {
+    if (err) {
+        console.log('There is some error in your code')
+        return;
+    }
+    console.log('Cool! Your File Has Been Created')
+})
